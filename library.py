670c09a14176fb28aca66a1f5838a2c8d10f6eaa
Alice <119490190+Alisyyyn@users.noreply.github.com>
2024-11-21 13:18:06 +0300

    Update library.py
    добавление новой функции сохранение данных в файл

--- library.py
+++ library.py
@@ -22,3 +22,21 @@ class Library:
             if book.title.lower() == title.lower():
                 return book
         return None
+          def save_to_file(self, filename):
+        data = {
+            "books": [book.to_dict() for book in self.books],
+            "users": [user.to_dict() for user in self.users]
+        }
+        with open(filename, "w") as file:
+            json.dump(data, file, indent=4)
+        print(f"Library data saved to '{filename}'.")
+
+    def load_from_file(self, filename):
+        try:
+            with open(filename, "r") as file:
+                data = json.load(file)
+            self.books = [Book.from_dict(book) for book in data["books"]]
+            self.users = [User.from_dict(user) for user in data["users"]]
+            print(f"Library data loaded from '{filename}'.")
+        except FileNotFoundError:
+            print(f"File '{filename}' not found.")
